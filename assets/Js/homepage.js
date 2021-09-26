73a9bfd9662acfdfe0a39a94b08919c7a47c6170
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-26 19:00:06 -0400

    Able to search repos from a input form

--- assets/Js/homepage.js
+++ assets/Js/homepage.js
@@ -1,12 +1,31 @@
-var getUserRepos = function (user){
-    //format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-    
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-    console.log(data);
-});
-});
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+} else {
+    alert("Please enter a Github username");
+}
+    console.log(event);
 };
-getUserRepos("azuryte5");
+
+var getUserRepos = function (user) {
+  //format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
