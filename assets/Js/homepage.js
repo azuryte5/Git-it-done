00701bde7bd3b52be078d1c84b80c48ae71d463f
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-26 19:44:34 -0400

    Error check done. div's show up now

--- assets/Js/homepage.js
+++ assets/Js/homepage.js
@@ -1,5 +1,10 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+//later when we build the presentation logic we need to build these two variables below
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -21,11 +26,76 @@ var getUserRepos = function (user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
+fetch(apiUrl)
+    .then(function (response) {
+    //ADD this to not get an 404 error
+    if(response.ok){
+        response.json().then(function(data) {
+            displayRepos(data,user);
+        });
+    } else {
+        alert("Error:GitHub User Not Found");
+    }
+    })
+    // Now, if the server is down. ADD this line. ontop of everything else
+    .catch(function(error) {
+        //Notice this '.catch()' getting chained onto the end of the '.then()' method
+    alert("Unable to connect to GitHub");
     });
-  });
+
+// This was ^ before we added the 404 ok check   
+//  response.json().then(function (data) {
+//       displayRepos(data, user);
+//     });
+//   });
 };
+
+var displayRepos = function(repos, searchTerm) {
+    // NOW what happens if length is 0. 
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+    // console.log(repos);
+    // console.log(searchTerm);
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    //loop generating rows of data 
+    // ANdrew, THis is what I needed to build my assignemnt 5 faster with the id codes that I probably could have controlled the id's
+for (var i=0; i < repos.length; i++) {
+    //format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+    // create a container for each repo
+    var repoEl =document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent= repoName;
+
+      //append to container
+      repoEl.appendChild(titleEl);
+    
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    //check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>"+ repos[i].open_issues_count + " issue(s)";
+    } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append damn.
+    repoEl.appendChild(statusEl)
+
+  
+    //append ^ container to the dom (Page)
+    repoContainerEl.appendChild(repoEl);
+}
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
